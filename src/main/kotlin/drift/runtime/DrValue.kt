a66bb35d2555a4209e5c1319a4ae727a79539252
belicfr <96206878+belicfr@users.noreply.github.com>
2025-06-12 17:14:31 -0400

    feat: implement types into first Drift type expressions and functions

--- src/main/kotlin/drift/runtime/DrValue.kt
+++ src/main/kotlin/drift/runtime/DrValue.kt
@@ -4,37 +4,58 @@ import drift.ast.DrStmt
 import drift.ast.FunctionParameter
 
 sealed interface DrValue {
-    fun asString(): String
+    fun asString() : String
+
+    fun type() : DrType
 }
 
 data class DrString(val value: String) : DrValue {
     override fun asString() = value
+
+    override fun type() = StringType
 }
 
 data class DrInt(val value: Int) : DrValue {
     override fun asString() = value.toString()
+
+    override fun type() = IntType
 }
 
 data class DrBool(val value: Boolean) : DrValue {
     override fun asString() = value.toString()
+
+    override fun type() = BoolType
 }
 
 data class DrFunction(
     val params: List<FunctionParameter>,
     val body: List<DrStmt>,
-    val closure: DrEnv) : DrValue {
+    val closure: DrEnv,
+    val returnType: DrType = AnyType) : DrValue {
 
     override fun asString(): String = "function"
+
+    override fun type(): DrType = FunctionType(
+        params.map { it.type },
+        returnType)
 }
 
 data class DrNativeFunction(
-    val impl: (List<Pair<String?, DrValue>>) -> DrValue) : DrValue {
+    val impl: (List<Pair<String?, DrValue>>) -> DrValue,
+    val paramTypes: List<DrType>,
+    val returnType: DrType = AnyType) : DrValue {
 
     override fun asString(): String = "native function"
+
+    override fun type(): DrType = FunctionType(
+        paramTypes,
+        returnType)
 }
 
 object DrNull : DrValue {
     override fun asString() = "null"
+
+    override fun type(): DrType = NullType
 }
 
 
