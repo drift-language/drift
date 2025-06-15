71c5ede467c5821882e92d293c24dbf23041bbcb
belicfr <96206878+belicfr@users.noreply.github.com>
2025-06-14 22:56:38 -0400

    feat: create Lambda and Unary expressions

--- src/main/kotlin/drift/ast/DrExpr.kt
+++ src/main/kotlin/drift/ast/DrExpr.kt
@@ -1,6 +1,7 @@
 package drift.ast
 
 import drift.runtime.DrEnv
+import drift.runtime.DrType
 import drift.runtime.DrValue
 
 sealed interface DrExpr
@@ -10,5 +11,7 @@ data class Variable(val name: String) : DrExpr
 data class Call(val callee: DrExpr, val args: List<Argument>) : DrExpr
 data class Binary(val left: DrExpr, val operator: String, val right: DrExpr) : DrExpr
 data class Conditional(val condition: DrExpr, val thenBranch: DrStmt, val elseBranch: DrStmt?) : DrExpr
+data class Lambda(val parameters: List<FunctionParameter>, val body: List<DrStmt>, val returnType: DrType) : DrExpr
+data class Unary(val operator: String, val expr: DrExpr) : DrExpr
 
 data class Argument(val name: String?, val expr: DrExpr)
