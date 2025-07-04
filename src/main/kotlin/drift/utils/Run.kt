432434c4b7d06cbbb6a3ac2ba78dde9f5bac81b9
belicfr <96206878+belicfr@users.noreply.github.com>
2025-07-04 02:02:57 -0400

    test: add evaluation with output capturing
    Adds functionality to evaluate code and capture any output generated during execution.
    
    This allows for testing and debugging of code that produces output, enabling the verification of expected results and the identification of potential errors.

--- src/main/kotlin/drift/utils/Run.kt
+++ src/main/kotlin/drift/utils/Run.kt
@@ -3,9 +3,7 @@ package drift.utils
 import drift.ast.eval
 import drift.parser.Parser
 import drift.parser.lex
-import drift.runtime.DrEnv
-import drift.runtime.DrNull
-import drift.runtime.DrValue
+import drift.runtime.*
 
 fun evalProgram(source: String) : DrValue {
     val tokens = lex(source)
@@ -19,4 +17,30 @@ fun evalProgram(source: String) : DrValue {
     }
 
     return result
+}
+
+fun evalWithOutputs(source: String) : MutableList<String> {
+    val output = mutableListOf<String>()
+
+    val tokens = lex(source)
+    val statements = Parser(tokens).parse()
+    val env = DrEnv().apply {
+        define("test", DrNativeFunction(
+            impl = { args ->
+                args.map { output.add(it.second.asString()) }
+                DrNull
+            },
+            paramTypes = listOf(AnyType),
+            returnType = NullType))
+    }
+
+    for (statement in statements) {
+        statement.eval(env)
+    }
+
+    return output
+}
+
+fun evalWithOutput(source: String) : String {
+    return evalWithOutputs(source).last()
 }
