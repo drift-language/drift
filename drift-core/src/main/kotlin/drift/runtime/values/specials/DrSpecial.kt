ec2135f0cf3a40cdaa2c893bfcd1d62e18f158c0
belicfr <96206878+belicfr@users.noreply.github.com>
2025-09-25 11:57:39 +0200

    refactor: split special types in many files

---
+++ drift-core/src/main/kotlin/drift/runtime/values/specials/DrSpecial.kt
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * Drift Programming Language                                                 *
+ *                                                                            *
+ * Copyright (c) 2025. Jonathan (GitHub: belicfr)                             *
+ *                                                                            *
+ * This source code is licensed under the MIT License.                        *
+ * See the LICENSE file in the root directory for details.                    *
+ ******************************************************************************/
+package drift.runtime.values.specials
+
+import drift.runtime.DrValue
+
+
+/******************************************************************************
+ * DRIFT SPECIAL RUNTIME TYPE
+ *
+ * Interface for all special types.
+ ******************************************************************************/
+
+
+
+/**
+ * Runtime interface representing all Drift special types.
+ */
+interface DrSpecial : DrValue
--- drift-core/src/main/kotlin/drift/runtime/values/specials/DrVoid.kt
+++ drift-core/src/main/kotlin/drift/runtime/values/specials/DrVoid.kt
@@ -6,33 +6,18 @@
  * This source code is licensed under the MIT License.                        *
  * See the LICENSE file in the root directory for details.                    *
  ******************************************************************************/
+
 package drift.runtime.values.specials
 
-import drift.runtime.*
+import drift.runtime.DrValue
+import drift.runtime.VoidType
+
 
 /******************************************************************************
- * DRIFT SPECIAL VALUE TYPES STRUCTURES
+ * DRIFT VOID RUNTIME SPECIAL TYPE
  *
- * All special value types are defined in this file.
- * A special value type apply its behavior on runtime evaluation.
+ * Runtime class for Void special type.
  ******************************************************************************/
-
-
-
-/**
- * AST representation of the NULL type, which represents
- * the absence of a value in Drift.
- *
- * It is used for variables or expressions that do not
- * reference any object or value.
- *
- * @see NullType
- */
-data object DrNull : DrValue {
-    override fun asString() = "null"
-
-    override fun type(): DrType = NullType
-}
 
 
 
@@ -40,23 +25,10 @@ data object DrNull : DrValue {
  * AST representation of the VOID type, which represents
  * the absence of return for a function.
  *
- * @see VoidType
+ * @see drift.runtime.VoidType
  */
 data object DrVoid : DrValue {
     override fun asString() = "void"
 
     override fun type() = VoidType
-}
-
-
-
-/**
- * AST representation of the NotAssigned type, which represents
- * a variable which does not have a value.
- *
- * It is linked to [UnknownType].
- */
-data object DrNotAssigned : DrValue {
-    override fun asString(): String = UnknownType.asString()
-    override fun type(): DrType = UnknownType
 }
