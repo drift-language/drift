7fe7f8a868da1bc19a4c1c328c2c578c6c07f863
belicfr <96206878+belicfr@users.noreply.github.com>
2025-09-26 09:05:34 +0200

    feat: replace stub by project config for import path root dir

--- drift-core/src/main/kotlin/drift/parser/statements/ParseStatements.kt
+++ drift-core/src/main/kotlin/drift/parser/statements/ParseStatements.kt
@@ -314,7 +314,6 @@ internal fun Parser.parseImport() : Import {
 
     return Import(
         namespaceSteps.joinToString("."),
-        "src/${namespaceSteps.joinToString("/")}.drift"
+        "${config.structure.root}/${namespaceSteps.joinToString("/")}.drift"
     )
-    // TODO: use dynamic structure from drift.json
 }
