0e1d19be6e3cdc7cef58fa9622dd0e9b131f1289
belicfr <96206878+belicfr@users.noreply.github.com>
2025-11-30 23:19:06 +0100

    fix: wrong operators precedences

--- drift-core/src/main/kotlin/drift/parser/Parser.kt
+++ drift-core/src/main/kotlin/drift/parser/Parser.kt
@@ -62,29 +62,29 @@ class Parser(
     internal val operatorPrecedence: Map<String, Int> = mapOf(
         "="     to 1,
 
-        "><"    to 2,
+        "&&"    to 2,
 
-        ".."    to 3,
-        "..<"   to 3,
+        "||"    to 3,
 
-        "?"     to 4,
+        "><"    to 4,
 
-        "=="    to 5,
-        "!="    to 5,
-        ">"     to 5,
-        "<"     to 5,
-        ">="    to 5,
-        "<="    to 5,
+        ".."    to 5,
+        "..<"   to 5,
 
-        "+"     to 6,
-        "-"     to 6,
+        "?"     to 6,
 
-        "*"     to 7,
-        "/"     to 7,
+        "=="    to 7,
+        "!="    to 7,
+        ">"     to 8,
+        "<"     to 8,
+        ">="    to 8,
+        "<="    to 8,
 
-        "&&"    to 8,
+        "+"     to 9,
+        "-"     to 9,
 
-        "||"    to 9,
+        "*"     to 10,
+        "/"     to 10,
     )
 
 
