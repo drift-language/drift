b96707604d9d79c6664890d3101d7c5fa9041299
belicfr <96206878+belicfr@users.noreply.github.com>
2026-01-05 16:55:15 +0100

    refactor: create exception classes to replace globale ones

---
+++ drift-core/src/main/kotlin/drift/lexer/exceptions/DriftLexerExceptions.kt
@@ -0,0 +1,74 @@
+/******************************************************************************
+ * Drift Programming Language                                                 *
+ *                                                                            *
+ * Copyright (c) 2025. Jonathan (GitHub: belicfr)                             *
+ *                                                                            *
+ * This source code is licensed under the MIT License.                        *
+ * See the LICENSE file in the root directory for details.                    *
+ ******************************************************************************/
+package drift.lexer.exceptions
+
+import drift.exceptions.DriftException
+import drift.lexer.Token
+
+
+/******************************************************************************
+ * DRIFT LEXER EXCEPTIONS
+ *
+ * All Drift Lexer exception classes are defined in this file.
+ ******************************************************************************/
+
+
+
+/**
+ * Drift exception to throw on error on lexing.
+ *
+ * @property message
+ * @property token The token that caused the exception, if applicable
+ * @property sourceName The file where the exception has been thrown
+ * @property line The line where the error is
+ * @property pos The character position where the error is
+ */
+open class DriftLexerException(
+    val token: Token? = null,
+    message: String,
+    sourceName: String? = null,
+    line: Int = 0,
+    pos: Int = 0)
+    : DriftException(message, sourceName, line, pos)
+
+
+
+/**
+ * Drift Lexer exception to throw if an unexpected character
+ * has been found.
+ *
+ * @property unexpected Unexpected character found
+ * @property sourceName The file where the exception has been thrown
+ * @property line The line where the error is
+ * @property pos The character position where the error is
+ */
+class DLUnexpectedCharacterException(
+    unexpected: Char,
+    sourceName: String? = null,
+    line: Int = 0,
+    pos: Int = 0
+) : DriftLexerException(
+    message = "Unexpected character '$unexpected'",
+    sourceName = sourceName,
+    line = line,
+    pos = pos
+)
+
+
+
+class DLUnterminatedStringLiteralException(
+    sourceName: String? = null,
+    line: Int = 0,
+    pos: Int = 0
+) : DriftLexerException(
+    message = "Unterminated string literal",
+    sourceName = sourceName,
+    line = line,
+    pos = pos
+)
---
+++ drift-core/src/main/kotlin/drift/runtime/exceptions/DriftModuleLoaderExceptions.kt
@@ -0,0 +1,75 @@
+/******************************************************************************
+ * Drift Programming Language                                                 *
+ *                                                                            *
+ * Copyright (c) 2025. Jonathan (GitHub: belicfr)                             *
+ *                                                                            *
+ * This source code is licensed under the MIT License.                        *
+ * See the LICENSE file in the root directory for details.                    *
+ ******************************************************************************/
+package drift.runtime.exceptions
+
+import jdk.internal.joptsimple.internal.Messages.message
+
+
+/******************************************************************************
+ * DRIFT MODULE LOADER EXCEPTIONS
+ *
+ * All Drift Module Loader exception classes are defined in this file.
+ ******************************************************************************/
+
+
+
+open class DriftModuleLoaderException(
+    message: String,
+    sourceName: String? = null,
+    line: Int = 0,
+    pos: Int = 0,
+) : DriftRuntimeException(
+    message = message,
+    sourceName = sourceName,
+    line = line,
+    pos = pos,
+)
+
+
+
+class DMLAlreadyImportedModuleException(
+    moduleNamespace: String,
+    sourceName: String? = null,
+    line: Int = 0,
+    pos: Int = 0,
+) : DriftModuleLoaderException(
+    message = "Module $moduleNamespace is already imported",
+    sourceName = sourceName,
+    line = line,
+    pos = pos,
+)
+
+
+
+class DMLUnexistingModuleException(
+    moduleNamespace: String,
+    sourceName: String? = null,
+    line: Int = 0,
+    pos: Int = 0,
+) : DriftModuleLoaderException(
+    message = "Module $moduleNamespace does not exist",
+    sourceName = sourceName,
+    line = line,
+    pos = pos,
+)
+
+
+
+class DMLNotFoundInModuleException(
+    element: String,
+    moduleNamespace: String,
+    sourceName: String? = null,
+    line: Int = 0,
+    pos: Int = 0,
+) : DriftModuleLoaderException(
+    message = "'$element' not found in module $moduleNamespace",
+    sourceName = sourceName,
+    line = line,
+    pos = pos,
+)
