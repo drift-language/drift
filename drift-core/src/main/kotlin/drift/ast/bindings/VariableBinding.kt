6d7a56bdd8a2b040930b45c2a7874ee808123750
belicfr <96206878+belicfr@users.noreply.github.com>
2026-02-02 13:28:43 +0100

    feat: create VariableBinding

---
+++ drift-core/src/main/kotlin/drift/ast/bindings/VariableBinding.kt
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * Drift Programming Language                                                 *
+ *                                                                            *
+ * Copyright (c) 2026. Jonathan (GitHub: belicfr)                             *
+ *                                                                            *
+ * This source code is licensed under the MIT License.                        *
+ * See the LICENSE file in the root directory for details.                    *
+ ******************************************************************************/
+package drift.ast.bindings
+
+import drift.ast.ParserNode
+
+
+abstract class VariableBinding(
+    open val name: String) : ParserNode()
