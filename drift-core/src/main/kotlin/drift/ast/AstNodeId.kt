b12cfda3a6e4434a545837a30ad39a1a803e225a
belicfr <96206878+belicfr@users.noreply.github.com>
2026-02-02 13:29:14 +0100

    feat: create root ParserNode entity and implement NodeId generation

---
+++ drift-core/src/main/kotlin/drift/ast/AstNodeId.kt
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * Drift Programming Language                                                 *
+ *                                                                            *
+ * Copyright (c) 2026. Jonathan (GitHub: belicfr)                             *
+ *                                                                            *
+ * This source code is licensed under the MIT License.                        *
+ * See the LICENSE file in the root directory for details.                    *
+ ******************************************************************************/
+package drift.ast
+
+object AstNodeId {
+
+    private var current: Int = 0
+
+
+    fun allocate(): Int = current++
+}
