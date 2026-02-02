c407d50b79e8c62a66217824414bc90d1733b700
belicfr <96206878+belicfr@users.noreply.github.com>
2026-02-02 13:28:28 +0100

    feat: create ForVariable

---
+++ drift-core/src/main/kotlin/drift/ast/bindings/ForVariable.kt
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * Drift Programming Language                                                 *
+ *                                                                            *
+ * Copyright (c) 2026. Jonathan (GitHub: belicfr)                             *
+ *                                                                            *
+ * This source code is licensed under the MIT License.                        *
+ * See the LICENSE file in the root directory for details.                    *
+ ******************************************************************************/
+package drift.ast.bindings
+
+import drift.runtime.AnyType
+import drift.runtime.ParserType
+
+
+class ForVariable(
+    override val name: String) : VariableBinding(name)
