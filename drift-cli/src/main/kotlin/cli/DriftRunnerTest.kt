3dcdee2d9ee2efa3de97f6ba0e50f1d664f6cfdd
belicfr <96206878+belicfr@users.noreply.github.com>
2026-02-23 18:51:44 +0100

    refactor: disable interpreter, add hir log and prepare for compiler

--- drift-cli/src/main/kotlin/cli/DriftRunnerTest.kt
+++ drift-cli/src/main/kotlin/cli/DriftRunnerTest.kt
@@ -14,9 +14,11 @@ import com.github.ajalt.mordant.rendering.TextColors.Companion.rgb
 import com.github.ajalt.mordant.rendering.TextStyles.*
 import com.github.ajalt.mordant.terminal.Terminal
 import drift.DriftVersion
-import drift.ast.statements.Function
+import drift.ast.statements.Func
 import drift.checkers.TypeChecker
 import drift.checkers.collectors.SymbolCollector
+import drift.hir.converter.ASTToHIRConverter
+import drift.ir.symbols.SymbolTable
 import drift.lexer.lex
 import drift.parser.Parser
 import drift.runtime.AnyType
@@ -71,52 +73,60 @@ fun main(args: Array<String>) {
 
     val ast = Parser(tokens).parse()
     t.println(bold(red("[AST]\t\t")) + italic(ast.toString()))
+
+    val hir = ASTToHIRConverter(
+        ast = ast,
+        symbolTable = SymbolTable(),
+        emptyMap(),
+        emptyMap()).convert()
+    t.println(bold(green("[HIR]\t\t")) + italic(hir.toString()))
+
     t.println("\n——————\n")
 
-    env.run {
-        define("print", ParserNativeFunction(
-            impl = { _, args ->
-                println(args.joinToString(" ") { it.second.asString() })
-                ParserVoid
-            },
-            paramTypes = listOf(AnyType),
-            returnType = NullType
-        ))
-
-        defineClass("String", ParserClass("String", mutableMapOf(), mutableMapOf(
-            "length" to ParserMethod(
-                let = Function(
-                    name = "length",
-                    parameters = emptyList(),
-                    returnType = ObjectType("Int"),
-                    body = emptyList()
-                ),
-                closure = env,
-                nativeImpl = ParserNativeFunction(
-                    name = "length",
-                    paramTypes = emptyList(),
-                    returnType = ObjectType("Int"),
-                    impl = { receiver, args ->
-                        val instance = receiver as ParserString
-
-                        ParserInt(instance.value.length)
-                    }
-                )
-            )
-        ), closure = env))
-
-        defineClass("Int", ParserClass("Int", mutableMapOf(), mutableMapOf(), closure = env))
-        defineClass("Bool", ParserClass("Bool", mutableMapOf(), mutableMapOf(), closure = env))
-        defineClass("Int64", ParserClass("Int64", mutableMapOf(), mutableMapOf(), closure = env))
-        defineClass("UInt", ParserClass("UInt", mutableMapOf(), mutableMapOf(), closure = env))
-    }
-
-    SymbolCollector(env).collect(ast)
-    TypeChecker(env).check(ast)
-
-    for (statement in ast) {
-        statement.eval(env)
-    }
+//    env.run {
+//        define("print", ParserNativeFunction(
+//            impl = { _, args ->
+//                println(args.joinToString(" ") { it.second.asString() })
+//                ParserVoid
+//            },
+//            paramTypes = listOf(AnyType),
+//            returnType = NullType
+//        ))
+//
+//        defineClass("String", ParserClass("String", mutableMapOf(), mutableMapOf(
+//            "length" to ParserMethod(
+//                let = Func(
+//                    name = "length",
+//                    parameters = emptyList(),
+//                    returnType = ObjectType("Int"),
+//                    body = emptyList()
+//                ),
+//                closure = env,
+//                nativeImpl = ParserNativeFunction(
+//                    name = "length",
+//                    paramTypes = emptyList(),
+//                    returnType = ObjectType("Int"),
+//                    impl = { receiver, args ->
+//                        val instance = receiver as ParserString
+//
+//                        ParserInt(instance.value.length)
+//                    }
+//                )
+//            )
+//        ), closure = env))
+//
+//        defineClass("Int", ParserClass("Int", mutableMapOf(), mutableMapOf(), closure = env))
+//        defineClass("Bool", ParserClass("Bool", mutableMapOf(), mutableMapOf(), closure = env))
+//        defineClass("Int64", ParserClass("Int64", mutableMapOf(), mutableMapOf(), closure = env))
+//        defineClass("UInt", ParserClass("UInt", mutableMapOf(), mutableMapOf(), closure = env))
+//    }
+//
+//    SymbolCollector(env).collect(ast)
+//    TypeChecker(env).check(ast)
+//
+//    for (statement in ast) {
+//        statement.eval(env)
+//    }
 
     t.run {
         println()
