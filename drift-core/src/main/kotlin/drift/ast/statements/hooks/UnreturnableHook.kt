e2caf0796619ebcf2ab2de74b3af25c857867a61
belicfr <96206878+belicfr@users.noreply.github.com>
2026-04-02 12:24:14 +0200

    feat: create hook nodes classes

---
+++ drift-core/src/main/kotlin/drift/ast/statements/hooks/UnreturnableHook.kt
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Drift Programming Language                                                 *
+ *                                                                            *
+ * Copyright (c) 2026. Jonathan (GitHub: belicfr)                             *
+ *                                                                            *
+ * This source code is licensed under the MIT License.                        *
+ * See the LICENSE file in the root directory for details.                    *
+ ******************************************************************************/
+
+package drift.ast.statements.hooks
+
+import drift.ast.ParserCallable
+import drift.ast.bindings.FunctionParameter
+import drift.ast.metadata.Annotation
+import drift.ast.statements.Block
+import drift.ast.statements.ParserStatement
+
+
+data class UnreturnableHook(
+    override val name: String,
+    override val annotations: MutableList<Annotation> = mutableListOf(),
+    override val parameters: List<FunctionParameter> = listOf(),
+    override val body: Block = Block.empty())
+    : ParserStatement(), ParserCallable, ParserHook
