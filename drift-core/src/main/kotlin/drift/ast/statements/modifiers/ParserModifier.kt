650a4925a4c49e68fd8fa26a0b3552b86365c1a7
belicfr <96206878+belicfr@users.noreply.github.com>
2026-04-02 12:24:30 +0200

    feat: create modifier nodes classes

---
+++ drift-core/src/main/kotlin/drift/ast/statements/modifiers/ParserModifier.kt
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * Drift Programming Language                                                 *
+ *                                                                            *
+ * Copyright (c) 2026. Jonathan (GitHub: belicfr)                             *
+ *                                                                            *
+ * This source code is licensed under the MIT License.                        *
+ * See the LICENSE file in the root directory for details.                    *
+ ******************************************************************************/
+
+package drift.ast.statements.modifiers
+
+
+/**
+ * A modifier adds crucial information
+ * for the compiler, on a statement.
+ *
+ * It affects the behavior of the compiler
+ * against the related statement, its effects,
+ * and result.
+ */
+sealed interface ParserModifier
---
+++ drift-core/src/main/kotlin/drift/ast/statements/modifiers/ParserNativeModifier.kt
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * Drift Programming Language                                                 *
+ *                                                                            *
+ * Copyright (c) 2026. Jonathan (GitHub: belicfr)                             *
+ *                                                                            *
+ * This source code is licensed under the MIT License.                        *
+ * See the LICENSE file in the root directory for details.                    *
+ ******************************************************************************/
+package drift.ast.statements.modifiers
+
+
+/**
+ * The native modifier permits defining as an exposition
+ * in Drift a language-defined function.
+ *
+ * This modifier is reserved for native libraries and
+ * should not be used in a custom project.
+ */
+object ParserNativeModifier : ParserModifier
